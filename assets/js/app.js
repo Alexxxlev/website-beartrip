f864b89f998a5580992c0a7fa2d02ecbfa3a309a
Alexander <102146514+Alexxxlev@users.noreply.github.com>
2022-07-19 22:52:20 +0300

    тестовое задание

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+// var dataCollapse = $("[data-collapse]");
+
+// /* Collapse */ 
+// dataCollapse.on("click", function(event) {
+//     event.preventDefault();
+
+//     var $this = $(this);
+
+//     $this.toggleClass("active");
+// });
+
+
+// event.stopPropagation()
