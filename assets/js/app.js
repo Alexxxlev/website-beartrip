c6b46c2725716bfe69e1b69c6a7b5a3744c59f13
Alexander <102146514+Alexxxlev@users.noreply.github.com>
2022-07-23 16:55:17 +0300

    тестовое задание

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,75 +2,135 @@ var navToggle = $("#navToggle"),
     header = $("#header"),
     menu = $("#menu"),
     dataSubmenu = $("[data-submenu]"),
-    submenu = $("#submenu");
+    submenu = $("#submenu"),
+    modal = $(".modal"),
+    modalDialog = $(".modal__dialog");
 
-/* Menu nav toggle */
+const modalCall = $("[data-modal]"),
+      modalClose = $("[data-close]");
+
+/* Menu nav toggle
+================================== */
 navToggle.on("click", function(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    navToggle.toggleClass("active");
-    header.toggleClass("active");
-    menu.toggleClass("active");
+  navToggle.toggleClass("active");
+  header.toggleClass("active");
+  menu.toggleClass("active");
 });
 
-/* Submenu */ 
+/* Submenu
+================================== */ 
 submenu.on("click", function(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    dataSubmenu.toggleClass("active");
+  dataSubmenu.toggleClass("active");
 });
 
 
-/* swiper */
+/* swiper
+================================== */
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 3,
-    slidesPerGroup: 1,
-    loopFillGroupWithBlank: true,
-  
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+  slidesPerView: 3,
+  spaceBetween: 3,
+  slidesPerGroup: 1,
+  loopFillGroupWithBlank: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+/* input +-
+================================== */
+jQuery(function() {
+  var j = jQuery;
+  var addInput1 = '#qty_1, #qty-header_1';
+  var n = 1;
+
+  j(addInput1).val(n);
+
+  j('.plus_1').on('click', function() {
+    j(addInput1).val(++n);
   });
 
-  /* input +- */
-  jQuery(function() {
-      var j = jQuery;
-      var addInput1 = '#qty_1, #qty-header_1';
-      var n = 1;
+  j('.min_1').on('click', function() {
+    if (n >=1) {
+      j(addInput1).val(--n);
+    } else {
 
-      j(addInput1).val(n);
-
-      j('.plus_1').on('click', function() {
-        j(addInput1).val(++n);
-      });
-
-      j('.min_1').on('click', function() {
-        if (n >=1) {
-          j(addInput1).val(--n);
-        } else {
-
-        }
-      });
-    });
+    }
+  });
+});
     
-  jQuery(function() {
-      var j = jQuery;
-      var addInput2 = '#qty_2, #qty-header_2';
-      var n = 0;
+jQuery(function() {
+  var j = jQuery;
+  var addInput2 = '#qty_2, #qty-header_2';
+  var n = 0;
 
-      j(addInput2).val(n);
+  j(addInput2).val(n);
 
-      j('.plus_2').on('click', function() {
-        j(addInput2).val(++n);
-      });
-
-      j('.min_2').on('click', function() {
-        if (n >=1) {
-          j(addInput2).val(--n);
-        } else {
-
-        }
-      });
+  j('.plus_2').on('click', function() {
+    j(addInput2).val(++n);
   });
+
+  j('.min_2').on('click', function() {
+    if (n >=1) {
+      j(addInput2).val(--n);
+    } else {
+
+    }
+  });
+});
+
+/* Modal
+================================== */
+modalCall.on("click", function(event) {
+  event.preventDefault();
+
+  let $this = $(this);
+  let modalId = $this.data('modal');
+
+  $(modalId).addClass('show');
+  $("body").addClass('no-scroll');
+
+  setTimeout(function() {
+    $(modalId).find(modalDialog).css({
+      transform: "scale(1)"
+    });
+  }, 200);
+});
+
+modal.on("click", function(event) {
+  let $this = $(this);
+
+  $this.find(modalDialog).css({
+    transform: "scale(0)"
+  });
+
+  setTimeout(function() {
+    $this.removeClass('show');
+    $("body").removeClass('no-scroll');
+  }, 200);
+
+  modalDialog.on("click", function(event) {
+    event.stopPropagation();
+  });
+});
+
+modalClose.on("click", function(event) {
+  event.preventDefault();
+
+  let $this = $(this);
+  let modalParent = $this.parents('.modal');
+
+  modalParent.find(modalDialog).css({
+    transform: "scale(0)"
+  });
+
+  setTimeout(function() {
+    modalParent.removeClass('show');
+    $("body").removeClass('no-scroll');
+  }, 200);
+});
